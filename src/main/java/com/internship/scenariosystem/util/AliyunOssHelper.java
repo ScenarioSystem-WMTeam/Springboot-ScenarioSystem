8eb9d578acdca5adbcd7fa4b95b12da34748a40c
AiBaeHyunee <52733373+AiBaeHyunee@users.noreply.github.com>
2021-06-24 08:16:11 +0800

    Update AliyunOssHelper.java

--- src/main/java/com/internship/scenariosystem/util/AliyunOssHelper.java
+++ src/main/java/com/internship/scenariosystem/util/AliyunOssHelper.java
@@ -22,8 +22,8 @@ public class AliyunOssHelper {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
         String endpoint = "oss-cn-beijing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tS6u8JyPRfxCX8R6FpJ";
-        String accessKeySecret = "cugj2wWQeWMyxKFKIHrGP4Yfn9sRH5";
+        String accessKeyId = "您的id";
+        String accessKeySecret = "您的密码";
 
         String urlPrefix = "https://systemdemo.oss-cn-beijing.aliyuncs.com/";
 
